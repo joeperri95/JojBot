b039b84e4e16ee0e08319ee0cbd1849f487c2746
Joe Perri <perrijs2@mcmaster.ca>
2018-07-24 19:25:05 -0400

    Added some music functionality

--- src/JojBot.py
+++ src/JojBot.py
@@ -1,3 +1,5 @@
+#run.py
+
 #JojBot discord bot
 
 BOT_PREFIX = '!'
@@ -10,8 +12,43 @@ from discord import Game
 from discord.ext.commands import Bot
 import random
 import os
-import requests
 import queue
+
+class bot:
+	def __init__(self):
+		self.connected = False
+		self.connection = None
+		self.musicQueue = queue.Queue()
+		self.currentPlayer = None
+
+	def vc_connect(self, channel):
+		if(self.isConnected()):
+			return
+		else:
+			self.connected = True
+			self.connection = channel
+
+	def vc_disconnect(self):
+		self.connected = False
+		self.connection = None
+
+	def enqueue(self, player):
+		#handle input 
+		self.musicQueue.put(player)
+
+	def dequeue(self):
+		return self.musicQueue.get()
+
+	def isConnected(self):
+		return self.connected
+
+	def playNext(self):
+		player = self.musicQueue.get()
+		player.play()
+
+
+b = bot()
+
 
 #change to root directory
 os.chdir("..")
@@ -35,14 +72,8 @@ async def bitcoin():
      url = 'http://api.coindesk.com/v1/bpi/currentprice/BTC.json'
      response = requests.get(url)
      value = response.json()['bpi']['USD']['rate']
-     await client.say("Bitcoin price is " + value)
-
-@client.command(name = "music", pass_context = True)
-async def music(context, link):
-    pass
-    
-    
-
+     await client.say("Bitcoin price is " + value)   
+ 
 @client.command(name="meme", description = "Get a random meme image", brief = "Grab a dank meme" , pass_context=True)
 async def meme(context):
     memeList = os.listdir(os.getcwd() + "/res/Memes")
@@ -56,15 +87,34 @@ async def octagon(context):
         videos = fp.read().split('\n')
     await client.say(random.choice(videos))
 
+@client.command(name = "mus", pass_context = True)
+async def mus(context):
+  channel = context.message.author.voice_channel
+  if(channel is None):
+  	await client.say("Enter a voice channel dummy " + context.message.author.mention)
+  	return
+  else:
+  	pass
+
+  if(b.isConnected()):
+  	link = context.message.content.strip('!mus ')
+  	player = await b.connection.create_ytdl_player(link, after = lambda: b.playNext())
+  	b.enqueue(player)
+  	await client.say("I enqueued it b")
+  	pass
+  
+  else:
+  	b.vc_connect(await client.join_voice_channel(channel))
+  	link = context.message.content.strip('!mus ')
+  	b.enqueue(await b.connection.create_ytdl_player(link, after = lambda: b.playNext()))
+  	b.dequeue().start()
+
 @client.event
 async def on_ready():
     with open(os.getcwd() + "/res/GameList.txt", "r") as gamelistfile:
         gamelist = gamelistfile.read().split('\n');
     gamechoice = random.choice(gamelist)
     await client.change_presence(game=Game(name=gamechoice))
-    await client.send_message(discord.Object('342738319819407370') , "Guess who's back")
-    print('Logged in as ' + client.user.name)    
+    print('Logged in as ' + client.user.name)
 
 client.run(secret.TOKEN)
-
-
